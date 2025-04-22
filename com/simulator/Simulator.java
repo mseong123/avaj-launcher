4208eb18f866138fe188b02b1dd4ef48e0b2019f
mseong123 <mseong123@hotmail.com>
2025-04-22 16:53:36 +0800

    done filereader

--- com/simulator/Simulator.java
+++ com/simulator/Simulator.java
@@ -1,13 +1,34 @@
 
 package com.simulator; 
 
-import com.simulator.weather.WeatherProvider;
+// import com.simulator.weather.WeatherProvider;
+import java.io.*;
+import java.io.IOException;
 
 public class Simulator {
     public static void main(String[] args) {
-        Coordinates coordinates = new Coordinates(600,5,299); 
-        WeatherProvider weatherprovider = WeatherProvider.getInstance();
-        String weather = weatherprovider.getCurrentWeather(coordinates);
-        System.out.println(weather);
+        // Coordinates coordinates = new Coordinates(600,5,299); 
+        // WeatherProvider weatherprovider = WeatherProvider.getInstance();
+        // String weather = weatherprovider.getCurrentWeather(coordinates);
+        // System.out.println(weather);
+        int simulationCount;
+        try(BufferedReader reader = new BufferedReader(new FileReader("scenario.txt"))) {
+            String line;
+            int lineCount = 0;
+            while((line = reader.readLine()) != null) {
+                if (lineCount++ == 0) 
+                    simulationCount = Integer.parseInt(line);
+                else {
+                   String[] arr = line.split("[\\s]");
+                   for (String s : arr)
+                        System.out.println(s);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+        
+
     }
 }
